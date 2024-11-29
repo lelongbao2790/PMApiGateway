d50d7949ecf6e20a8e01370b3030dadff53486dc
Bao <lelongbao2790@gmail.com>
2024-11-29 09:26:47 +0700

    Update port

--- obj/Debug/net8.0/PM-Api-Gateway.AssemblyInfo.cs
+++ obj/Debug/net8.0/PM-Api-Gateway.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PM-Api-Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd559caa5976afe42b54572e03834d9b353fc8d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PM-Api-Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PM-Api-Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
